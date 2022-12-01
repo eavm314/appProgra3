2c5ae30f261f5968b123959ebd6e1078973c4430
eavm <enriqueadhemar@gmail.com>
2022-12-01 11:17:42 -0400

    carpetitas

--- app/src/main/java/com/grupoa/sparkyoutofbounds/activities/MainActivity.kt
+++ app/src/main/java/com/grupoa/sparkyoutofbounds/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.grupoa.sparkyoutofbounds
+package com.grupoa.sparkyoutofbounds.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.grupoa.sparkyoutofbounds.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
