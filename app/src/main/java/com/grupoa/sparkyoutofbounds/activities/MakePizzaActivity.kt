fe207a66a4b5ff655498c38b5e2c2302196d7fe3
enrique <enriqueadhemar@gmail.com>
2022-12-08 08:55:12 -0400

    Radio Button de tamaño funciona

--- app/src/main/java/com/grupoa/sparkyoutofbounds/activities/MakePizzaActivity.kt
+++ app/src/main/java/com/grupoa/sparkyoutofbounds/activities/MakePizzaActivity.kt
@@ -3,13 +3,15 @@ package com.grupoa.sparkyoutofbounds.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.grupoa.sparkyoutofbounds.adapters.IngredientSelectAdapter
-import com.grupoa.sparkyoutofbounds.dataClasses.Ingrediente
+import com.grupoa.sparkyoutofbounds.dataClasses.Pizza
 import com.grupoa.sparkyoutofbounds.databinding.ActivityMakePizzaBinding
 
 class MakePizzaActivity : AppCompatActivity() {
+
+    val pizza: Pizza = Pizza()
 
     private lateinit var binding: ActivityMakePizzaBinding
 
@@ -25,16 +27,23 @@ class MakePizzaActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        setIngredients()
+        binding.radioButtons.setOnCheckedChangeListener { _, id ->
+            val size = when (id){
+                binding.pequenia.id -> 0.5
+                binding.regular.id -> 1.0
+                binding.grande.id -> 1.5
+                binding.interminable.id -> 2.0
+                else -> 1.0
+            }
+            pizza.size = size
+
+            Log.d("pizza", pizza.toString())
+        }
+        setIngredientSelect()
     }
 
-    fun setIngredients() {
-        val listaIngredientes = mutableListOf(
-            Ingrediente("Queso", 10, false),
-            Ingrediente("Carne", 20, true),
-            Ingrediente("Tocino", 15, true),
-            Ingrediente("Tomate", 5, false)
-        )
+    fun setIngredientSelect() {
+        val listaIngredientes = pizza.ingredients
 
         ingredientSelectAdapter.addIngredientSelects(listaIngredientes)
 
